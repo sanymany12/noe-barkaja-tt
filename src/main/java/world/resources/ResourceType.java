2b06c9a1130a6b2ce07e49cc6fcdb702966e05ab
nwul7d <nwul7d@inf.elte.hu>
2026-03-18 14:48:52 +0100

    Cargo implementálva az osztálydiagram szerint

--- src/main/java/world/resources/ResourceType.java
+++ src/main/java/world/resources/ResourceType.java
@@ -1,4 +1,24 @@
 package world.resources;
 
 public enum ResourceType implements ICargo{
+    GRAIN("Gabona", 0),
+    FOOD("Ételkonzerv", 1);
+
+    private String displayName;
+    private int value;
+
+    ResourceType(String name, int value) {
+        this.displayName = name;
+        this.value = value;
+    }
+
+    @Override
+    public String getDisplayName() {
+        return this.displayName;
+    }
+
+    @Override
+    public int getValue() {
+        return this.value;
+    }
 }
