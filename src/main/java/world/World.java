ac3b1576d41bdc390d75974545f51c2f5514d721
nwul7d <nwul7d@inf.elte.hu>
2026-03-18 15:17:49 +0100

    Kisebb kiegészítések a World-ben

--- src/main/java/world/World.java
+++ src/main/java/world/World.java
@@ -1,6 +1,8 @@
 package world;
 
 import world.building.Building;
+import world.tile.Point;
+import world.tile.TerrainType;
 import world.tile.Tile;
 import world.vehicle.Vehicle;
 
@@ -34,6 +36,16 @@ public class World {
         }
         else {
             return false;
+        }
+    }
+
+    public List<Point> findPath(Tile start, Tile stop) throws Exception {
+        if (stop.getTerrainType() != TerrainType.STOP || stop.getTerrainType() != TerrainType.ROAD) {
+            throw new Exception("The destination isn't on the road!");
+        }
+        else {
+            return null;
+            // pathfinding algorithm needed
         }
     }
 }
--- src/main/java/world/tile/Tile.java
+++ src/main/java/world/tile/Tile.java
@@ -1,4 +1,13 @@
 package world.tile;
 
 public class Tile {
+    private TerrainType terrainType;
+
+    public Tile() {
+
+    }
+
+    public TerrainType getTerrainType() {
+        return this.terrainType;
+    }
 }
