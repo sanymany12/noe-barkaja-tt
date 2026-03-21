f3fd0cba2fa31ada5dacd9aa04793540ef2fd05c
nwul7d <nwul7d@inf.elte.hu>
2026-03-21 14:03:13 +0100

    Utak megfelelő megjelenítéséhez metódus

--- src/main/java/world/tile/road/Road.java
+++ src/main/java/world/tile/road/Road.java
@@ -14,7 +14,9 @@ public class Road {
     }
 
     public void setConnection(RoadDirection dir) {
-        this.connections.add(dir);
+        if (!this.connections.contains(dir)) {
+            this.connections.add(dir);
+        }
     }
 
     public boolean canGo(RoadDirection dir) {
@@ -22,6 +24,55 @@ public class Road {
     }
 
     public String getSpriteName() {
-        return "TODO";
+        String spriteName = "road-";
+        spriteName.concat(String.valueOf(connections.toArray().length));
+        spriteName.concat("-");
+        if (canGo(RoadDirection.NORTH)) {
+            spriteName.concat("n");
+        }
+        if (canGo(RoadDirection.WEST)) {
+            spriteName.concat("w");
+        }
+        if (canGo(RoadDirection.SOUTH)) {
+            spriteName.concat("s");
+        }
+        if (canGo(RoadDirection.EAST)) {
+            spriteName.concat("e");
+        }
+        spriteName.concat(".png");
+        return spriteName;
+//        switch (connections.toArray().length) {
+//            case 4:
+//                return "road-4.png";
+//            case 3:
+//                if (!this.canGo(RoadDirection.NORTH)) {
+//                    return "road-3-wse.png";
+//                }
+//                else if (!this.canGo(RoadDirection.WEST)) {
+//                    return "road-3-nse.png";
+//                }
+//                else if (!this.canGo(RoadDirection.SOUTH)) {
+//                    return "road-3-nwe.png";
+//                }
+//                else return "road-3-nws.png";
+//            case 2:
+//                if (this.canGo(RoadDirection.NORTH)) {
+//
+//                }
+//            case 1:
+//                if (this.canGo(RoadDirection.NORTH)) {
+//                    return "road-1-n.png";
+//                }
+//                else if (this.canGo(RoadDirection.EAST)) {
+//                    return "road-1-e.png";
+//                }
+//                else if (this.canGo(RoadDirection.WEST)) {
+//                    return "road-1-w.png";
+//                }
+//                else return "road-1-s.png";
+//                break;
+//            default:
+//                return "road-0.png";
+//        }
     }
 }
