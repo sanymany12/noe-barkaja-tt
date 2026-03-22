733f30ed09d8eaa5ad6d2f34583c66fc790e12cf
bodim <k6oto7@inf.elte.hu>
2026-03-22 19:42:40 +0100

    MVC - controller implementacio

---
+++ src/main/java/controller/GameController.java
@@ -0,0 +1,148 @@
+package controller;
+
+import UI.ingameGUI;
+import engine.GameEngine;
+import engine.GameListener;
+import world.tile.Point;
+import world.tile.Tile;
+import world.tile.TerrainType;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
+import javax.swing.SwingUtilities;
+
+public class GameController implements GameListener {
+
+    private final GameEngine model;
+    private final ingameGUI view;
+
+    private int lastMouseX;
+    private int lastMouseY;
+
+    private int mapWidthTiles;
+    private int mapHeightTiles;
+
+    private enum BuildState { NONE, BUILD_ROAD}
+    private BuildState currentState = BuildState.NONE;
+
+    public GameController(GameEngine model, ingameGUI view)
+    {
+        this.model = model;
+        this.view = view;
+
+        this.model.start(this);
+
+        this.view.getMapPanel().setRenderer(this.model.getRenderer());
+
+        mapWidthTiles = model.getWorld().getCols();
+        mapHeightTiles = model.getWorld().getRows();
+
+        setupButtons();
+        setupMouseControl();
+    }
+
+    private void setupButtons()
+    {
+        view.getRoadToggle().addActionListener(e -> {
+            if(currentState == BuildState.BUILD_ROAD)
+            {
+                currentState = BuildState.NONE;
+                view.getRoadToggle().setText("Ut ikon");
+            }
+            else
+            {
+                currentState = BuildState.BUILD_ROAD;
+                view.getRoadToggle().setText("**Ut ikon**");
+            }
+        });
+    }
+
+    private void setupMouseControl()
+    {
+        MouseAdapter mouseAdapter = new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e)
+            {
+                if(SwingUtilities.isRightMouseButton(e))
+                {
+                    currentState = BuildState.NONE;
+                    view.getRoadToggle().setText("Ut ikon");
+                    return;
+                }
+
+                if(currentState == BuildState.BUILD_ROAD && SwingUtilities.isLeftMouseButton(e))
+                {
+                    buildRoadAtScreen(e.getX(), e.getY());
+                }
+                else if(currentState == BuildState.NONE && SwingUtilities.isLeftMouseButton(e))
+                {
+                    lastMouseX = e.getX();
+                    lastMouseY = e.getY();
+                }
+            }
+
+            @Override
+            public void mouseDragged(MouseEvent e)
+            {
+                if(currentState == BuildState.BUILD_ROAD && SwingUtilities.isLeftMouseButton(e))
+                {
+                    buildRoadAtScreen(e.getX(), e.getY());
+                }
+                else if(currentState == BuildState.NONE && SwingUtilities.isLeftMouseButton(e))
+                {
+                    int dx = e.getX() - lastMouseX;
+                    int dy = e.getY() - lastMouseY;
+
+                    model.getCamera().move(-dx, -dy, mapWidthTiles, mapHeightTiles);
+
+                    lastMouseX = e.getX();
+                    lastMouseY = e.getY();
+
+                    view.mapRefresh();
+                }
+
+            }
+
+            @Override
+            public void mouseWheelMoved(MouseWheelEvent e)
+            {
+                double zoomSensitivity = 0.2;
+                double currentZoom = model.getCamera().getZoom();
+                double newZoom = currentZoom - (e.getWheelRotation() * zoomSensitivity);
+
+                model.getCamera().setZoom(newZoom, mapWidthTiles, mapHeightTiles);
+
+                view.mapRefresh();
+            }
+        };
+
+        view.getMapPanel().addMouseListener(mouseAdapter);
+        view.getMapPanel().addMouseMotionListener(mouseAdapter);
+        view.getMapPanel().addMouseWheelListener(mouseAdapter);
+    }
+
+    private void buildRoadAtScreen(int screenX, int screenY)
+    {
+        Point gridPos = model.getCamera().screenToWorld(screenX, screenY);
+        Tile tile = model.getWorld().get(gridPos.x, gridPos.y);
+
+        if(tile != null && tile.getTerrainType() == TerrainType.LAND && tile.getBuilding() == null)
+        {
+            model.getWorld().buildRoad(tile);
+            view.mapRefresh();
+        }
+    }
+
+    @Override
+    public void onTick()
+    {
+        view.mapRefresh();
+    }
+
+    @Override
+    public void onNewDay(int currentDay)
+    {
+        view.setDay(currentDay);
+    }
+}
