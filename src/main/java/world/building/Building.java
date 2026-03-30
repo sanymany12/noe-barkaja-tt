970cac04c8dacda06babbc90632d64b3490176e7
nwul7d <nwul7d@inf.elte.hu>
2026-03-30 18:24:53 +0200

    Épületek bare minimum implementálása a Stationhez

--- src/main/java/world/building/Building.java
+++ src/main/java/world/building/Building.java
@@ -1,4 +1,23 @@
 package world.building;
 
+import world.tile.Tile;
+
 public abstract class Building<In, Out> {
+    protected int row;
+    protected int col;
+    protected BuildingType type;
+
+    public Building(Tile tile) {
+        this.col = tile.getCoordinate().x;
+        this.row = tile.getCoordinate().y;
+        this.type = null;
+    }
+
+    public BuildingType getBuildingType() {
+        return this.type;
+    }
+
+    public void update() {
+
+    }
 }
--- src/main/java/world/building/Enclosure.java
+++ src/main/java/world/building/Enclosure.java
@@ -1,6 +1,19 @@
 package world.building;
 
 import world.resources.AnimalType;
+import world.tile.Tile;
+import world.vehicle.AnimalTruck;
+import world.vehicle.Vehicle;
 
-public class Enclosure extends Building<AnimalType, AnimalType>{
+public class Enclosure extends Building<AnimalType, AnimalType> {
+    private AnimalType species;
+
+    public Enclosure(Tile tile) {
+        super(tile);
+        this.species = null;
+    }
+
+    public AnimalType getSpecies() {
+        return this.species;
+    }
 }
