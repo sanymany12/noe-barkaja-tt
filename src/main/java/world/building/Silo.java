9ecd3166e9649364ab37415f5d26e1e386acc2c1
nwul7d <nwul7d@inf.elte.hu>
2026-04-04 17:57:40 +0200

    Siló implementálása

--- src/main/java/world/building/Silo.java
+++ src/main/java/world/building/Silo.java
@@ -3,8 +3,30 @@ package world.building;
 import world.World;
 
 public class Silo extends Building<Integer,Integer> {
+    private int numOfFood;
+
+    private final int STARTING_STOCK = 20;
+    private final int CAPACITY = 500;
+
     public Silo(World world) {
         super(world);
+
+        this.numOfFood = STARTING_STOCK;
+    }
+
+    public int getNumOfFood() {
+        return this.numOfFood;
+    }
+
+    // A boolean visszaadja, volt-e elég étel a silóban.
+    public boolean consumeFood(int amount) {
+        if (amount <= numOfFood) {
+            numOfFood = numOfFood - amount;
+            return true;
+        } else {
+            numOfFood = 0;
+            return false;
+        }
     }
 
     @Override
