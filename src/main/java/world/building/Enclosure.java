c54e936740ce6651eafb4adc0f1f9c2f931c9f6d
nwul7d <nwul7d@inf.elte.hu>
2026-04-04 17:57:51 +0200

    Karám implementálása

--- src/main/java/world/building/Enclosure.java
+++ src/main/java/world/building/Enclosure.java
@@ -8,18 +8,47 @@ import world.vehicle.Vehicle;
 
 public class Enclosure extends Building<AnimalType, AnimalType> {
     private AnimalType species;
+    private int numOfAnimals;
+    private Silo silo;
+    private boolean starving;
 
-    public Enclosure(World world) {
+    private final double ANIMAL_MULTIPLIER = 1.2;
+    private final int CAPACITY = 100;
+
+    public Enclosure(World world, Silo silo) {
         super(world);
+
+        this.silo = silo;
 
         this.width = 2;
         this.height = 1;
 
         this.species = null;
+        this.numOfAnimals = 0;
+
+        this.starving = false;
     }
 
     public AnimalType getSpecies() {
         return this.species;
+    }
+
+    public int getNumOfAnimals() {
+        return this.numOfAnimals;
+    }
+
+    public void consumeFood() {
+        if (!this.silo.consumeFood(numOfAnimals)) {
+            this.starving = true;
+        } else {
+            this.starving = false;
+        }
+    }
+
+    public void animalBorn() {
+        if (this.numOfAnimals < this.CAPACITY && !this.starving) {
+            this.numOfAnimals++;
+        }
     }
 
     @Override
