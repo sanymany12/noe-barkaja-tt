36e1c7398e6dc29d5fa347a3b5c2afefd9a3278b
nwul7d <nwul7d@inf.elte.hu>
2026-04-04 19:17:17 +0200

    Farm implementálás

--- src/main/java/world/building/Farm.java
+++ src/main/java/world/building/Farm.java
@@ -4,9 +4,38 @@ package world.building;
 
 import world.World;
 
+import java.util.Random;
+
 public class Farm extends Building<Integer,Integer> {
+    private int grainMade;
+
+    private Random random;
+
+    private final int MIN_GAIN = 20;
+    private final int MAX_GAIN = 70;
+    private final int CAPACITY = 200;
+
     public Farm(World world) {
         super(world);
+
+        this.grainMade = 0;
+        this.random = new Random();
+    }
+
+    public void newDay() {
+        if (this.grainMade != this.CAPACITY) {
+            this.grainGrows();
+        }
+    }
+
+    private void grainGrows() {
+        int today = random.nextInt(MIN_GAIN, MAX_GAIN);
+
+        if (this.grainMade + today <= this.CAPACITY) {
+            this.grainMade = this.grainMade + today;
+        } else {
+            this.grainMade = this.CAPACITY;
+        }
     }
 
     @Override
