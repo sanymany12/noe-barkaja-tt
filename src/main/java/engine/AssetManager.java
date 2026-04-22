a0bdd8a35f2a4b1285743d2876fcb0e91f49d9d2
nwul7d <nwul7d@inf.elte.hu>
2026-04-22 13:18:36 +0200

    Silo asset használata

--- src/main/java/engine/AssetManager.java
+++ src/main/java/engine/AssetManager.java
@@ -182,6 +182,9 @@ public class AssetManager {
         // FARM sprite betöltése
         loadAsset("farm", "/assets-final/farm.png");
 
+        // SILO sprite betöltése
+        loadAsset("silo", "/assets-final/silo.png");
+
         loadAsset("building", "/assets/CP_V1.0.4_nyknck/CP_V1.0.4_01.png"); //épület 3x9
         loadAsset("concrete", "/assets/CP_V1.0.4_nyknck/CP_V1.0.4_58.png"); //beton cella
     }
