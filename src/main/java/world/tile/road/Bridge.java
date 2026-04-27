4b5818a6bb13d001b27db3807a1e91d1edcbd139
nwul7d <nwul7d@inf.elte.hu>
2026-04-28 00:10:17 +0200

    Hídépítésnek metódus írása

--- src/main/java/world/tile/road/Bridge.java
+++ src/main/java/world/tile/road/Bridge.java
@@ -5,12 +5,17 @@ public class Bridge extends Road {
 
     private int speedlimit;
 
-    public Bridge(int x, int y, BridgeType type) {
+    private RoadDirection direction;
+    private boolean isEnd;
+
+    public Bridge(int x, int y, BridgeType type, RoadDirection dir, boolean isEnd) {
         super(x, y);
 
         this.isBridge = true;
 
         this.type = type;
+        this.direction = dir;
+        this.isEnd = isEnd;
     }
 
     public BridgeType getType() {
