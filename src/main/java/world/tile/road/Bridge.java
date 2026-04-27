f3b9318bb1337d0ec5ce120c6b9bb48e6fb9aef9
nwul7d <nwul7d@inf.elte.hu>
2026-04-27 22:26:28 +0200

    Hidak alap implementálása

--- src/main/java/world/tile/road/Bridge.java
+++ src/main/java/world/tile/road/Bridge.java
@@ -1,7 +1,19 @@
 package world.tile.road;
 
 public class Bridge extends Road {
-    public Bridge(int x, int y) {
+    private BridgeType type;
+
+    private int speedlimit;
+
+    public Bridge(int x, int y, BridgeType type) {
         super(x, y);
+
+        this.isBridge = true;
+
+        this.type = type;
+    }
+
+    public BridgeType getType() {
+        return this.type;
     }
 }
--- src/main/java/world/tile/road/BridgeType.java
+++ src/main/java/world/tile/road/BridgeType.java
@@ -1,4 +1,17 @@
 package world.tile.road;
 
 public enum BridgeType {
+    WOOD(70, 3, 1),
+    STONE(80, 5, 5),
+    GLASS(150, 10, 20);
+
+    private int cost;
+    private int maxlength;
+    private int speedlimit;
+
+    BridgeType(int cost, int maxlength, int speedlimit) {
+        this.cost = cost;
+        this.maxlength = maxlength;
+        this.speedlimit = speedlimit;
+    }
 }
